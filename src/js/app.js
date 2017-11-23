56751b0bb19ae4508700bb5d4c42c888d8696116
Anton Suprunov <suprunov.anton@gmail.com>
2017-11-23 21:58:11 +0500

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,19 @@
 import $ from 'jquery';
 
-
 const onDomReady = () => {
-
+  setDynamicElementsHeights();
 };
 
+const setDynamicElementsHeights = () => {
+  $('.news-block').height($('.news-block').width());
+  $('.catalog-img').height($('.catalog-img').width());
+
+  $('.menu-open').on('click', (e) => {
+    $('.menu').addClass('menu_active');
+  });
+  $('.menu-close').on('click', (e) => {
+    $('.menu').removeClass('menu_active');
+  });
+};
 
 $(onDomReady);
